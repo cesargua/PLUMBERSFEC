84ebac5e1a4c6d7a7b922ed10da428ac096e2a08
cesar guajardo <cesarguajardo@cesars-MacBook-Pro.local>
2023-06-05 19:47:17 -0700

    amplify

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 const app = require('./index');
-const awsServerlessExpress = require('aws-serverless-express');
+// const awsServerlessExpress = require('aws-serverless-express');
 
-const server = awsServerlessExpress.createServer(app);
+// const server = awsServerlessExpress.createServer(app);
 
-exports.handler = (event, context) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
-  awsServerlessExpress.proxy(server, event, context);
-};
+// exports.handler = (event, context) => {
+//   console.log(`EVENT: ${JSON.stringify(event)}`);
+//   awsServerlessExpress.proxy(server, event, context);
+// };
 
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server available at http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server available at http://localhost:${PORT}`);
+});
